aaf46ca0a597e514b43cf4652c85afb32281af75
Tiago <tiago.spana@gmail.com>
2018-11-28 17:20:36 -0200

    correção da criação do bd

--- manutVeiculo/Database.cs
+++ manutVeiculo/Database.cs
@@ -14,132 +14,135 @@ namespace manutVeiculo
 {
     class Database
     {
-    
-            private static SQLiteConnection conexao;
-            private static Database instance;
+
+        private static SQLiteConnection conexao;
+        private static Database instance;
         private const string URL = "Data Source=manutVeiculo.db; foreign keys = true;";
 
         ///private const string URL = "File: C:/Test/manutVeiculo.sqlite";
         ///
-        
+
 
 
         private Database()
+        {
+            if (!File.Exists("manutVeiculo.db"))
             {
-              if (!File.Exists("manutVeiculo.db"))
-                {
-                    CreateDB();
-                }
-                conexao = new SQLiteConnection(URL);
+                CreateDB();
             }
-            public static Database GetInstance()
+            conexao = new SQLiteConnection(URL);
+        }
+        public static Database GetInstance()
+        {
+            if (instance == null)
             {
-                if (instance == null)
-                {
-                    instance = new Database();
-                }
-                return instance;
+                instance = new Database();
             }
-            public SQLiteConnection GetConnection()
+            return instance;
+        }
+        public SQLiteConnection GetConnection()
+        {
+            return conexao;
+        }
+        public void ExecuteSQL(string qry)
+        {
+            if (conexao.State != System.Data.ConnectionState.Open)
             {
-                return conexao;
+                conexao.Open();
             }
-            public void ExecuteSQL(string qry)
+            SQLiteCommand comm = new SQLiteCommand(qry, conexao);
+            comm.ExecuteNonQuery();
+            conexao.Close();
+        }
+        private void CreateDB()
+        {
+            SQLiteConnection.CreateFile("manutVeiculo.db");
+            SQLiteConnection conn = new SQLiteConnection(URL);
+            conn.Open();
+
+            StringBuilder sql = new StringBuilder();
+
+            sql.AppendLine("CREATE TABLE IF NOT EXISTS pessoa (");
+            sql.AppendLine("[id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+            sql.AppendLine("[cpf] VARCHAR(20) NOT NULL, ");
+            sql.AppendLine("[nome] VARCHAR(100), ");
+            sql.AppendLine("[sexo] VARCHAR(1), ");
+            sql.AppendLine("[rua] VARCHAR(100), ");
+            sql.AppendLine("[bairro] VARCHAR(50), ");
+            sql.AppendLine("[numero] INTEGER, ");
+            sql.AppendLine("[cep] VARCHAR(20), ");
+            sql.AppendLine("[cidade] VARCHAR(50), ");
+            sql.AppendLine("[uf] VARCHAR(2)); ");
+
+            SQLiteCommand cmd = new SQLiteCommand(sql.ToString(), conn);
+            try
             {
-                if (conexao.State != System.Data.ConnectionState.Open)
-                {
-                    conexao.Open();
-                }
-                SQLiteCommand comm = new SQLiteCommand(qry, conexao);
-                comm.ExecuteNonQuery();
-                conexao.Close();
+                cmd.ExecuteNonQuery();
             }
-            private void CreateDB()
+            catch (Exception ex)
             {
-                SQLiteConnection.CreateFile("manutVeiculo.db");
-                SQLiteConnection conn = new SQLiteConnection(URL);
-                conn.Open();
+                MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
+            }
 
-                StringBuilder sql = new StringBuilder();
+            sql.Clear();
 
-                sql.AppendLine("CREATE TABLE IF NOT EXISTS pessoa (");
-                sql.AppendLine("[id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
-                sql.AppendLine("[cpf] VARCHAR(20) NOT NULL, ");
-                sql.AppendLine("[nome] VARCHAR(100), ");
-                sql.AppendLine("[sexo] VARCHAR(1), ");
-                sql.AppendLine("[rua] VARCHAR(100), ");
-                sql.AppendLine("[bairro] VARCHAR(50), ");
-                sql.AppendLine("[numero] INTEGER, ");
-                sql.AppendLine("[cep] VARCHAR(20), ");
-                sql.AppendLine("[cidade] VARCHAR(50), ");
-                sql.AppendLine("[uf] VARCHAR(2)); ");
-
-                SQLiteCommand cmd = new SQLiteCommand(sql.ToString(), conn);
-                try
-                {
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
-                }
-
-                sql.Clear();
-
-                sql.AppendLine("CREATE TABLE IF NOT EXISTS veiculo(");
-                sql.AppendLine("[id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
-                sql.AppendLine("[idCliente] VARCHAR(20) NOT NULL FOREIGN KEY(id) REFERENCES pessoa(id), ");
-                sql.AppendLine("[marca] VARCHAR(50), ");  
-                sql.AppendLine("[modelo] VARCHAR(50), ");
-                sql.AppendLine("[combustivel] VARCHAR(20), ");
-                sql.AppendLine("[placa] VARCHAR(50) , ");
-                sql.AppendLine("[kmRodado] INTEGER, ");
-                sql.AppendLine("[ano] INTEGER) ");
+            sql.AppendLine("CREATE TABLE IF NOT EXISTS veiculo(");
+            sql.AppendLine("[id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
+            sql.AppendLine("[idCliente] VARCHAR(20) NOT NULL, ");
+            sql.AppendLine("[marca] VARCHAR(50), ");
+            sql.AppendLine("[modelo] VARCHAR(50), ");
+            sql.AppendLine("[combustivel] VARCHAR(20), ");
+            sql.AppendLine("[placa] VARCHAR(50) , ");
+            sql.AppendLine("[kmRodado] INTEGER, ");
+            sql.AppendLine("[ano] INTEGER, ");
+            sql.AppendLine("FOREIGN KEY(idCliente) REFERENCES pessoa(id));");
 
                 cmd = new SQLiteCommand(sql.ToString(), conn);
 
-                try
-                {
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
-                }
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
+            }
 
-                sql.Clear();
+            sql.Clear();
 
-                sql.AppendLine("CREATE TABLE IF NOT EXISTS peca(");
-                sql.AppendLine("id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,");
-                sql.AppendLine("marca VARCHAR(50),");
-                sql.AppendLine("pecaServico VARCHAR(100),");
-                sql.AppendLine("modelo VARCHAR(50),");
-                sql.AppendLine("kmTroca VARCHAR(20),");
-                sql.AppendLine("preco FLOAT)");
+            sql.AppendLine("CREATE TABLE IF NOT EXISTS peca(");
+            sql.AppendLine("id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,");
+            sql.AppendLine("marca VARCHAR(50),");
+            sql.AppendLine("pecaServico VARCHAR(100),");
+            sql.AppendLine("modelo VARCHAR(50),");
+            sql.AppendLine("kmTroca VARCHAR(20),");
+            sql.AppendLine("preco FLOAT);");
 
-                cmd = new SQLiteCommand(sql.ToString(), conn);
+            cmd = new SQLiteCommand(sql.ToString(), conn);
 
-                try
-                {
-                    cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
-                }
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao criar banco de dados: " + ex.Message);
+            }
 
             sql.Clear();
 
 
             sql.AppendLine("CREATE TABLE IF NOT EXISTS os(");
             sql.AppendLine("id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ");
-            sql.AppendLine("cliente VARCHAR(50) FOREIGN KEY (id) REFERENCES pessoa(id), ");
-            sql.AppendLine("placa VARCHAR(20) FOREIGN KEY (placa) REFERENCES veiculo (placa), ");
+            sql.AppendLine("cliente INTEGER, ");
+            sql.AppendLine("placa VARCHAR(20), ");
             sql.AppendLine("status BOOLEAN, ");
             sql.AppendLine("peca VARCHAR(100), ");
             sql.AppendLine("valor VARCHAR(50), ");
             sql.AppendLine("km INTEGER, ");
-            sql.AppendLine("data DATE )");
+            sql.AppendLine("data DATE,");
+            sql.AppendLine("FOREIGN KEY (cliente) REFERENCES pessoa(id),");
+            sql.AppendLine("FOREIGN KEY (placa) REFERENCES veiculo (placa));");
 
             cmd = new SQLiteCommand(sql.ToString(), conn);
             try
@@ -154,10 +157,12 @@ namespace manutVeiculo
             sql.Clear();
 
             sql.AppendLine("CREATE TABLE IF NOT EXISTS consultaOs(");
-            sql.AppendLine("numeroOs INTEGER NOT NULL PRIMARY KEY FOREIGN KEY (id) REFERENCES os (id),");
-            sql.AppendLine("cliente INTEGER FOREIGN KEY (id) REFERENCES pessoa (id),");
+            sql.AppendLine("numeroOs INTEGER NOT NULL PRIMARY KEY,");
+            sql.AppendLine("cliente INTEGER,");
             sql.AppendLine("placa VARCHAR(50),");
-            sql.AppendLine("status BOOLEAN)");
+            sql.AppendLine("status BOOLEAN,");
+            sql.AppendLine("FOREIGN KEY (cliente) REFERENCES pessoa (id),");
+            sql.AppendLine("FOREIGN KEY (numeroOs) REFERENCES os (id));");
 
             cmd = new SQLiteCommand(sql.ToString(), conn);
 
@@ -171,7 +176,7 @@ namespace manutVeiculo
             }
 
             conn.Close();
-            }
+        }
     }
 }
 
